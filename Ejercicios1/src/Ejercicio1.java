472ede3c83945aa333ccc4da71e68d16c3aea80a
oscar <oscarmagallon2002@hotmail.com>
2020-11-04 12:30:38 +0100

    inicio

--- Ejercicios1/src/Ejercicio1.java
+++ Ejercicios1/src/Ejercicio1.java
@@ -1,27 +1,9 @@
-import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		
-		//Declaracion de variables
-		int num1=0,num2=2;
-		float real=0;
-		String nombre="";
-		boolean bandera=true;
-		
-		Scanner leer = new Scanner(System.in);
-		
-		System.out.println("Dime tu nombre");
-		nombre = leer.nextLine();
-		System.out.println("Dime tu edad");
-		num1 =leer.nextInt();
-		
-		
-		System.out.println("tu nombre es " + nombre + " Y tienes " + num1 + " años");
-		
-		
+		// TODO Auto-generated method stub
+
 	}
-	
 
 }
